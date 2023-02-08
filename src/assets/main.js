8b48c7df4dcf0fef1560612cdb9ba4feaf28dc3c
Ansony <ansonymendezsuarez@gmail.com>
2023-02-08 09:08:09 -0600

    first api querys

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,8 +2,8 @@ const API = 'https://pokeapi.co/api/v2/pokemon/';
 
 const content = null || document.getElementById('pokedex');
 
-async function getPokemons() {
-    const response = await fetch(`${API}`);
+async function getPokemons(urlAPI) {
+    const response = await fetch(urlAPI);
     const data = await response.json();
     return data;
 }
@@ -11,14 +11,13 @@ async function getPokemons() {
 (async () => {
     try {
         const pokemons = await getPokemons(API);
-        const pokemon = await getPokemons(`${API}/${pokemons.results[0].name}`);
+        const pokemon = await getPokemons(`${API}${pokemons.results[0].name}`);
 
         let view = `
-            ${pokemon.results.map(pokemon => `
-            <ul>
-                <li> ${pokemon.name} </li>
-                <li> ${pokemon.url} </li>
-            </ul>
+            ${pokemons.results.map(pokemon => `
+                <div class="card">
+                    <p> ${pokemon.name} </p>
+                </div>
             `).slice(0, 10).join('')}            
         `;
         content.innerHTML = view;
